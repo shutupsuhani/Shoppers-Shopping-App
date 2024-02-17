74b0fb990bf8149e26d7f27659ef705afdff2207
Suhani Sahu <itsscriptsiren>
2024-02-17 09:32:41 +0530

    ui update

--- client/frontend/src/pages/Login.jsx
+++ client/frontend/src/pages/Login.jsx
@@ -1,70 +1,56 @@
-//login.jsx
-import { Link,useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Topbar from '../components/Topbar';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-  
-  const Login = () => {
-   
-    
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [registeredUsers, setRegisteredUsers] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-    const navigate=useNavigate();
-  
-    useEffect(() => {
-      const storedUsers = localStorage.getItem('registeredUsers');
-     
-    
-      if (storedUsers) {
-        setRegisteredUsers(JSON.parse(storedUsers));
-      }
-    }, []);
-    
-  
-    const handleLogin = (e) => {
-      
-      e.preventDefault();
 
-      const user = registeredUsers.find(
-        (user) => user.username === username
-      );
-    
-      if (user && user.password === password) {
-        // Successful login
-        toast.success(`Welcome back, ${user.username}!`);
-       
-        setErrorMessage(''); // Clear any previous error message
-       
-        navigate("/home");
-      } else {
-        // Failed login
-        setErrorMessage('Invalid username or password');
-        toast.error('Login failed. Invalid username or password.');
-      }
-    
-     
-  
-    };
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [registeredUsers, setRegisteredUsers] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
 
-  
- 
-  
+  useEffect(() => {
+    const storedUsers = localStorage.getItem('registeredUsers');
+
+    if (storedUsers) {
+      setRegisteredUsers(JSON.parse(storedUsers));
+    }
+  }, []);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const user = registeredUsers.find((user) => user.username === username);
+
+    if (user && user.password === password) {
+      // Successful login
+      toast.success(`Welcome back, ${user.username}!`);
+
+      setErrorMessage(''); // Clear any previous error message
+
+      navigate("/home");
+    } else {
+      // Failed login
+      setErrorMessage('Invalid username or password');
+      toast.error('Login failed. Invalid username or password.');
+    }
+  };
+
   return (
     <>
       <Topbar />
 
-      <div className="bg-gray-300 font-mono dark:bg-gray-900 flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="h-screen bg-gray-300 font-mono flex flex-col items-center justify-center px-6 py-8">
+        <div className="w-full bg-white rounded-lg shadow md:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-mono text-center font-bold leading-tight tracking-tight text-pink-500 md:text-2xl dark:text-white">
               LOGIN
             </h1>
             {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}
             <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
-            <div>
+              <div>
                 <label htmlFor="username" className="block mb-2 text-md font-medium text-gray-900 dark:text-white text-left">
                   Username
                 </label>
@@ -74,8 +60,8 @@ import 'react-toastify/dist/ReactToastify.css';
                   id="username"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
-                  className=" border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required="true"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required={true}
                 />
               </div>
               <div>
@@ -90,7 +76,7 @@ import 'react-toastify/dist/ReactToastify.css';
                   onChange={(e) => setPassword(e.target.value)}
                   placeholder="Enter Password"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required="true"
+                  required={true}
                 />
               </div>
               <div className="flex items-center justify-between">
@@ -101,7 +87,7 @@ import 'react-toastify/dist/ReactToastify.css';
                       aria-describedby="remember"
                       type="checkbox"
                       className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      required=""
+                      required={true}
                     />
                   </div>
                   <div className="ml-3 text-sm">
@@ -114,21 +100,20 @@ import 'react-toastify/dist/ReactToastify.css';
                   Forgot password?
                 </a>
               </div>
-            <button
+              <button
                 type="submit"
-                className="w-full font-mono  text-white bg-gradient-to-r from-pink-500 border to-purple-500  hover:text-black  focus:ring-2 focus:outline-none focus:ring-blue-500 font-medium rounded-lg text-lg px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="w-full font-mono text-white bg-gradient-to-r from-pink-500 border to-purple-500 hover:text-black focus:ring-2 focus:outline-none focus:ring-blue-500 font-medium rounded-lg text-lg px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Sign in
-              </button> 
+              </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet?{' '}
-               <Link to="/register"><p>Sign up</p></Link>
-                
+                <Link to="/register">Sign up</Link>
               </p>
             </form>
           </div>
         </div>
-        <ToastContainer/>
+        <ToastContainer />
       </div>
     </>
   );
